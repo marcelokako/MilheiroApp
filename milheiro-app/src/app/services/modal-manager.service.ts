7409667a1cfe453d051c6a5f1d7f884c061b08b5
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2024-12-21 19:18:37 -0300

    ADD Feature de usar pontos

--- milheiro-app/src/app/services/modal-manager.service.ts
+++ milheiro-app/src/app/services/modal-manager.service.ts
@@ -6,6 +6,7 @@ import { PlataformaModalComponent } from '../components/plataforma-modal/platafo
 import { AddPontosModalComponent } from '../components/add-pontos-modal/add-pontos-modal.component';
 import { Plataforma } from './database.service';
 import { CalculadoraModalComponent } from '../components/calculadora-modal/calculadora-modal.component';
+import { UsarPontosModalComponent } from '../components/usar-pontos-modal/usar-pontos-modal.component';
 
 @Injectable({
   providedIn: 'root',
@@ -53,6 +54,24 @@ export class ModalManagerService {
 
   openCalculadoraModal(data?: Plataforma): Observable<any> {
     const dialogRef = this.dialog.open(CalculadoraModalComponent, {
+      width: '60%',
+      data: data || {},
+    });
+
+    return dialogRef.afterClosed();
+  }
+
+  openUsarPontosModal(
+    data?: { 
+      selectPessoa_option?: any,
+      plataformaSelecionada?: Plataforma,
+      pontos?: number,
+      custo_ponto?: any,
+      custo_equivalente?: any,
+      data_aquisicao?: string,
+      descricao?: string,
+    }): Observable<any> {
+    const dialogRef = this.dialog.open(UsarPontosModalComponent, {
       width: '60%',
       data: data || {},
     });
